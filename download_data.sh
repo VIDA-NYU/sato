c72ded3ad90868e76197639a2648688a53055b67
Remi Rampin <r@remirampin.com>
2019-12-05 17:03:12 -0500

    Update download_data.sh script

--- download_data.sh
+++ download_data.sh
@@ -1,17 +1,11 @@
-cd $BASEPATH
+#!/bin/sh
+
+set -eux
+
+cd "$(dirname "$0")"
 
 data_URL="http://sato-data.s3.amazonaws.com"
 
-curl "$data_URL/tmp.zip" > tmp.zip
-unzip tmp.zip
-
-curl "$data_URL/pretrained.zip" > sherlock/pretrained.zip
-cd sherlock; unzip pretrained.zip; cd -
-
-curl "$data_URL/LDA_cache.zip" > topic_model/LDA_cache.zip
-cd topic_model; unzip LDA_cache.zip; cd -
-
-# clean up
-rm tmp.zip
-rm sherlock/pretrained.zip
-rm topic_model/LDA_cache.zip
+curl -Lo tmp.zip "$data_URL/tmp.zip" > tmp.zip
+curl -Lo sherlock/pretrained.zip "$data_URL/pretrained.zip"
+curl -Lo sato/topic_model/LDA_cache.zip "$data_URL/LDA_cache.zip"
