dc1710df5b8c14db2636a88ca9b93aa8fb1a3738
Heiko Mueller <heiko.muller@gmail.com>
2020-08-11 13:58:02 -0400

    Basic support for CSV and TSV files

--- sato/cli.py
+++ sato/cli.py
@@ -16,23 +16,48 @@ from sato.predict import evaluate
     type=click.Path(file_okay=True, dir_okay=False, exists=True)
 )
 def run_predict(count, src):
-    """Predict column types for tab-delimited CSV file(s)."""
+    """Predict column types for CSV file(s)."""
     for filename in src:
+        # This is a very basic attempt to determine the file compression and
+        # delimiter from the suffix. Currently, the following four oprions are
+        # recognized: '.csv', '.csv.gz', '.tsv', '.tsv.gz'. Files ending with
+        # '.gz' are assumed to be compressed by 'gzip' all other files are
+        # considered as uncompressed. The delimiter for '.csv' files is ',' and
+        # for '.tsv' files the delimiter is '\t'.
+        if filename.endswith('.csv'):
+            compression = None
+            delimiter = ','
+        elif filename.endswith('.csv.gz'):
+            compression = 'gzip'
+            delimiter = ','
+        elif filename.endswith('.tsv'):
+            compression = None
+            delimiter = '\t'
+        elif filename.endswith('.tsv.gz'):
+            compression = 'gzip'
+            delimiter = '\t'
+        else:
+            raise ValueError('unrecognized file format')
         try:
             df = pd.read_csv(
                 filename,
-                delimiter='\t',
-                compression='gzip',
+                delimiter=delimiter,
+                compression=compression,
                 low_memory=False
             )
             rows = df.shape[0]
-            if rows > 0:
-                if rows > count:
-                    df = df.sample(n=count, random_state=1)
-                labels = evaluate(df)
-                print('\n{}'.format(filename))
-                for i in range(len(df.columns)):
-                    print('%s\t%s' % (df.columns[i], labels[i]))
+            print('\n{}'.format(filename))
+            print('{}'.format('-' * len(filename)))
+            if rows == 0:
+                # Skip empty files.
+                continue
+            if rows > count:
+                # Take sample for large files.
+                df = df.sample(n=count, random_state=1)
+            # Evaluate data frame to get predicted coluumn labels.
+            labels = evaluate(df)
+            for i in range(len(df.columns)):
+                print('%s: %s' % (df.columns[i], labels[i]))
         except Exception as ex:
             print('error {}'.format(ex))
 
